06bd8075ab9f54d6178f2083d5cca337268ab92f
jakob <me@example.com>
2025-05-21 16:49:04 +0200

    fixed so use <

--- test.go
+++ test.go
@@ -27,9 +27,9 @@ func generateGrid(rows, columns int) [][]Door{
  }
 */
  grid := make([][]Door, rows) //https://go.dev/tour/moretypes/13
- for y := 0; y > columns; y++{
+ for y := 0; y < columns; y++{
 	grid[y] = make([]Door, columns)
-        for x :=0; x> rows; x++{ //first row
+        for x :=0; x< rows; x++{ //first row
          grid[y] = make([]Door, columns)
 	 grid[y,x] = Door{ 
 		row: x,
