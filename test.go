ac65365e4482f26b3c2d9cb82718ae3bc511fb64
jakob <me@example.com>
2025-05-21 17:10:45 +0200

    now fill paths it can go

--- test.go
+++ test.go
@@ -41,6 +41,21 @@ func generateGrid(rows, columns int) [][]Door{
         // door[x,y].up= &door[y,x]
         }
  }
+//now i have the grid i could assign the paths
+ for x :=0; x< rows; x++{ //first row
+  grid[0][x].wall = false;
+  grid[0][x].right= &grid[0][x+1]
+  grid[0][x].down= &grid[1][x+1] 
+  //ther is no up on first row
+}
+ for y:= 1; y < columns; y++{
+        for x:=y-1; x> rows; x++{
+         grid[x][y].wall = false;
+         grid[x][y].right= &grid[y][x+1]
+         grid[x][y].down= &grid[y+1][x+1]
+         grid[x][y].up= &grid[y-1][x+1]
+        }
+ }
 
 return grid
 }
