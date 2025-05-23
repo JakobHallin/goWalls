2e73310b85a901d15a002e0f894c97e3b5753ebf
jakob <me@example.com>
2025-05-23 17:43:00 +0200

    trying to understand package

--- test.go
+++ test.go
@@ -184,6 +184,31 @@ func printGrid(grid [][]Door){
   }
 }
 
+//testfunction
+
+func TestPathEnumeration(t *testing.T) {
+	cases := []struct{ cols, rows, paths int }{
+		{0, 0, 0},
+		{1, 1, 1},
+		{1, 2, 2},
+		{2, 1, 1},
+		{2, 2, 4},
+		{3, 3, 17},
+		{5, 3, 99},
+		{5, 4, 178},
+		{5, 5, 259},
+		{6, 4, 466},
+	}
+	for _, test := range cases {
+		var res [][]int
+		res = ConnectedPaths(test.cols, test.rows)
+		if len(res) != test.paths {
+			t.Errorf("wrong number of paths for grid %dx%d, expected %d, got %d",
+				test.cols, test.rows, test.paths, len(res))
+		}
+	}
+}
+
 func main() {
     fmt.Println("hello world")
     grid:= generateGrid(4,4)
@@ -226,6 +251,7 @@ var res [][]int
 res = ConnectedPaths(2,2)
 fmt.Println(len(res))
  
+TestPathEnumeration(grid)
 }
 
 
